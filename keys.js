71f09a52fb43f1c65a549121f32acb493b2c49aa
lilsakabighurt <khalilhall1221@outlook.com>
2020-01-24 23:47:10 -0500

    make call to ombd api

--- keys.js
+++ keys.js
@@ -4,3 +4,10 @@ exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
+
+function movieInfo(moviename){
+    axios.get(`https://www.omdbapi.com/?t=${moviename}&apikey=trilogy`)
+  .then(function(res) {
+    console.log(res.data);
+  })
+}movieInfo("Titanic");
--- liri.js
+++ liri.js
@@ -1,3 +1,12 @@
+const axios = require("axios");
 dotenv = require("dotenv").config();
-var key = require("./keys.js");
-var spotify = new spotify(keys.spotify);
+// var key = require("./keys.js");
+// var spotify = new spotify(keys.spotify);
+
+//API call to ombd api//
+function movieInfo(moviename){
+    axios.get(`https://www.omdbapi.com/?t=${moviename}&apikey=trilogy`)
+  .then(function(res) {
+    console.log(res.data);
+  })
+}movieInfo("Titanic");
