19dc254d55ed6b9f83b61787950f44c25cbddf18
lilsakabighurt <khalilhall1221@outlook.com>
2020-01-26 22:25:21 -0500

    Get required response from omdb api

--- liri.js
+++ liri.js
@@ -8,34 +8,50 @@ var spotify = new Spotify(keys.spotify);
 function movieInfo(moviename){
     axios.get(`https://www.omdbapi.com/?t=${moviename}&apikey=trilogy`)
   .then(function(res) {
-    console.log(res.data);
+    // console.log(res.data);
+    //Title//
+    console.log("Movie Title: " + res.data.Title);
+    //Year//
+    console.log("Year Released: " + res.data.Year);
+    //imdb rating//
+    console.log("IMDB Rating: " + res.data.imdbRating);
+    //rotten tomatoes rating//
+    console.log("Rotten Tomatoes Rating: " + res.data.Ratings[1].Value);
+    //language//
+    console.log("Language: " + res.data.Language);
+    //Production Company//
+    console.log("Produced By: " + res.data.Production);
+    //plot//
+    console.log("Movie Plot: " + res.data.Plot);
+    //actors//
+    console.log("Actors: " + res.data.Actors);
   })
 }
 
-function spotifyCall(songname){
-    spotify.search({ type: 'track', query: songname }, function(err, data) {
-        if (err) {
-          return console.log("Could not find song with this name.");
-        }
+// function spotifyCall(songname){
+//     spotify.search({ type: 'track', query: songname }, function(err, data) {
+//         if (err) {
+//           return console.log("Could not find song with this name.");
+//         }
        
-      //for loop to loop through items array.. if search has no value will return error message//
-      if(data.tracks.items.length != 0){
-      for(i = 0; i <data.tracks.items.length; i++){
-          //song name//
-          console.log("Song Name: " + data.tracks.items[i].name);
-          //artist name//
-          console.log("Artist Name: " + data.tracks.items[i].artists[0].name);
-          // preview link//
-          console.log("Preview Link: " + data.tracks.items[i].preview_url);
-          //album name//
-          console.log("Album Name: " + data.tracks.items[i].album.name);
-          // console.log(data.tracks.items[i].artists);
-      }
-    }else{
-        console.log("no results for your queuery")
-    }
-      });
-}
+//       //for loop to loop through items array.. if search has no value will return error message//
+//       if(data.tracks.items.length != 0){
+//       for(i = 0; i <data.tracks.items.length; i++){
+//           //song name//
+//           console.log("Song Name: " + data.tracks.items[i].name);
+//           //artist name//
+//           console.log("Artist Name: " + data.tracks.items[i].artists[0].name);
+//           // preview link//
+//           console.log("Preview Link: " + data.tracks.items[i].preview_url);
+//           //album name//
+//           console.log("Album Name: " + data.tracks.items[i].album.name);
+//           // console.log(data.tracks.items[i].artists);
+//       }
+//     }else{
+//         console.log("no results for your queuery")
+//     }
+//       });
+// }
   
 //remember will have to use process.argv
 function start(arg1, arg2){
