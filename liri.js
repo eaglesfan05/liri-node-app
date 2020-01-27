e79e95107aae916b0d2a7bb99c23ddb7ff3ed966
lilsakabighurt <khalilhall1221@outlook.com>
2020-01-27 16:51:05 -0500

    do what it says command will read from random.txt

--- liri.js
+++ liri.js
@@ -7,18 +7,20 @@ var moment = require("moment");
 moment().format()
 var fs = require('fs');
 
-// function read(){
-// fs.readFile("random.txt", "utf8", function(error, data) {
+function read(){
+    fs.readFile("random.txt", "utf8", function(error, data) {
 
-//     if (error) {
-//     console.log(error);
-//     }
-  
-//     console.log(data);
-  
-//   });
+        if (error) {
+        console.log(error);
+        }
+        let thisshit = data.split(",")
+        console.log(thisshit);
+        start(thisshit[0], JSON.parse(thisshit[1]));
+      });
+    
+}
 
-// }read();
+
 
 function bands(bandname){
     axios.get(`https://rest.bandsintown.com/artists/${bandname}/events?app_id=codingbootcamp`)
@@ -28,7 +30,8 @@ function bands(bandname){
     }).then(res => {
         //loop through artist events//
         for(i = 0; i<res.data.length; i++){
-            console.log(res.data[i]);
+           
+            // console.log(res.data[i]);
             //venue name//
             console.log("Venue Name: " + res.data[i].venue.name);
             //venue city//
@@ -40,59 +43,62 @@ function bands(bandname){
         
     })
 }
-// //API call to ombd api//
-// function movieInfo(moviename){
-//     axios.get(`https://www.omdbapi.com/?t=${moviename}&apikey=trilogy`)
-//   .then(function(res) {
-//     // console.log(res.data);
-//     //Title//
-//     console.log("Movie Title: " + res.data.Title);
-//     //Year//
-//     console.log("Year Released: " + res.data.Year);
-//     //imdb rating//
-//     console.log("IMDB Rating: " + res.data.imdbRating);
-//     //rotten tomatoes rating//
-//     console.log("Rotten Tomatoes Rating: " + res.data.Ratings[1].Value);
-//     //language//
-//     console.log("Language: " + res.data.Language);
-//     //Production Company//
-//     console.log("Produced By: " + res.data.Production);
-//     //plot//
-//     console.log("Movie Plot: " + res.data.Plot);
-//     //actors//
-//     console.log("Actors: " + res.data.Actors);
-//   })
-// }
-
-// function spotifyCall(songname){
-//     spotify.search({ type: 'track', query: songname }, function(err, data) {
-//         if (err) {
-//           return console.log("Could not find song with this name.");
-//         }
-       
-//       //for loop to loop through items array.. if search has no value will return error message//
-//       if(data.tracks.items.length != 0){
-//       for(i = 0; i <data.tracks.items.length; i++){
-//           //song name//
-//           console.log("Song Name: " + data.tracks.items[i].name);
-//           //artist name//
-//           console.log("Artist Name: " + data.tracks.items[i].artists[0].name);
-//           // preview link//
-//           console.log("Preview Link: " + data.tracks.items[i].preview_url);
-//           //album name//
-//           console.log("Album Name: " + data.tracks.items[i].album.name);
-//           // console.log(data.tracks.items[i].artists);
-//       }
-//     }else{
-//         console.log("no results for your queuery")
-//     }
-//       });
-// }
-const spotifyCall = async(str) => {
-    const data = await spotify.search({type: 'track', query: str})
-    let artist = (data.tracks.items[0].artists[0].name);
-    console.log(artist);
+//API call to ombd api//
+function movieInfo(moviename){
+    axios.get(`https://www.omdbapi.com/?t=${moviename}&apikey=trilogy`)
+  .then(res =>{
+         // console.log(res.data);
+    //Title//
+    console.log("Movie Title: " + res.data.Title);
+    //Year//
+    console.log("Year Released: " + res.data.Year);
+    //imdb rating//
+    console.log("IMDB Rating: " + res.data.imdbRating);
+    //rotten tomatoes rating some return error because no rotten tomatoes rating//
+    console.log("Rotten Tomatoes Rating: " + res.data.Ratings[1].Value);
+    //language//
+    console.log("Language: " + res.data.Language);
+    //Production Company//
+    console.log("Produced By: " + res.data.Production);
+    //plot//
+    console.log("Movie Plot: " + res.data.Plot);
+    //actors//
+    console.log("Actors: " + res.data.Actors);
+  }).catch(err =>{
+      console.log("That's not a movie champ");
+      process.exit(1);
+  })
 }
+
+function spotifyCall(songname){
+    spotify.search({ type: 'track', query: songname }, function(err, data) {
+        if (err) {
+          return console.log("Could not find song with this name.");
+        }
+       
+      //for loop to loop through items array.. if search has no value will return error message//
+      if(data.tracks.items.length != 0){
+      for(i = 0; i <data.tracks.items.length; i++){
+          //song name//
+          console.log("Song Name: " + data.tracks.items[i].name);
+          //artist name//
+          console.log("Artist Name: " + data.tracks.items[i].artists[0].name);
+          // preview link//
+          console.log("Preview Link: " + data.tracks.items[i].preview_url);
+          //album name//
+          console.log("Album Name: " + data.tracks.items[i].album.name);
+          // console.log(data.tracks.items[i].artists);
+      }
+    }else{
+        console.log("no results for your queuery")
+    }
+      });
+}
+// const spotifyCall = async(str) => {
+//     const data = await spotify.search({type: 'track', query: str})
+//     let artist = (data.tracks.items[0].artists[0].name);
+//     console.log(artist);
+// }
   
 
 //remember will have to use process.argv
@@ -105,7 +111,8 @@ if(arg1 === "spotify-this"){
 }else if(arg1 === "concert-this"){
     bands(arg2);
 }else if(arg1 === "do-what-it-says"){
-
+    read();
+    console.log("is working")
 } else{
     console.log("Try again bruh");
     process.exit(1);
